2da47e06401f40ed58560032ec7dc929530a8e2b
RenaissanceZwy <zhaowenyi96@163.com>
2016-09-29 21:44:44 +0800

    third

--- Main.java
+++ Main.java
@@ -25,7 +25,7 @@ public class Main {
         return length;  
     }  
     public static void main(String[] args) {  
-        String str=" ";  
+        String str="abcabcbb";  
         System.out.println(getSubStringLength(str));  
     }  
   
